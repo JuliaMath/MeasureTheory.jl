9757d04b7686b4918d68e91bfb45af26a79ff204
Chad Scherrer <chad.scherrer@gmail.com>
2020-01-11 14:52:09 -0800

    early edits

--- src/Measures.jl
+++ src/Measures.jl
@@ -6,6 +6,7 @@ import Distributions
 
 abstract type Measure{X} end
 
+export domain
 domain(::Measure{X}) where {P,X} = X
 domain(::Type{Measure{X}}) where {P,X} = X
 
@@ -20,24 +21,19 @@ domain(::Type{Measure{X}}) where {P,X} = X
 # Normal(μ,σ) = Normal(μ=μ,σ=σ)
 # for default names
 export @measure
-macro measure(expr)
-    @match expr begin
-        d => begin
-            d = esc(d)
-            return quote
-                struct $d{P,X} <: Measure{X}
-                    par :: P
-                end
-
-                $d(;kwargs...) = $d((;kwargs...))
-            end
+macro measure(d)
+    d = esc(d)
+    return quote
+        struct $d{P,X} <: Measure{X}
+            par :: P
         end
-    end
-    
+        $d(;kwargs...) = $d((;kwargs...))
+    end    
 end
 
 
-
+export baseMeasure
+export logdensity
 
 @trait Density{M,X} where {X = domain{M}} begin
     baseMeasure :: [M] => Measure{X}
@@ -50,13 +46,30 @@ end
 
 # Lebesgue measure
 
+export Lebesgue
 struct Lebesgue{X} <: Measure{X} end
 Lebesgue(X) = Lebesgue{X}()
 
 # Normal distribution
 
 export Normal
-@measure Normal
+
+# @measure Normal
+
+struct Normal{P,X} <: Measure{X}
+    par :: P
+end
+
+Normal(;kwargs...) = Normal((;kwargs...))
+
+
+
+function Normal(μ::Pμ, σ::Pσ) where {Pμ <: Real, Pσ <: Real}
+    let T = promote_type(Float64, Pμ, Pσ)
+        NT = NamedTuple{(:μ, :σ),Tuple{T, T}}
+        Normal{NT,T}((μ = T(μ), σ = T(σ)))
+    end
+end
 
 baseMeasure(::Type{Normal{P,X}}) where {P,X} = Lebesgue(X)
 
@@ -64,7 +77,7 @@ baseMeasure(::Type{Normal{P,X}}) where {P,X} = Lebesgue(X)
 
 @implement Density{Normal{P,X},X} where {X, P <: NamedTuple{(:μ, :σ)}} begin
     baseMeasure(d) = Lebesgue(X)
-    logdensity(d, x) = -log(d.σ) - log(2)/2 - log(π)/2 - (x - d.μ)^2 / (2 * d.σ^2)
+    logdensity(d, x) = -log(d.par.σ) - log(2)/2 - log(π)/2 - (x - d.par.μ)^2 / (2 * d.par.σ^2)
 end
 
 end # module
