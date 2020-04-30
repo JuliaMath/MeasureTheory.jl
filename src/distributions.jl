3441255f04990f945d138c62afc5867e8e361ae1
Chad Scherrer <chad.scherrer@gmail.com>
2020-04-29 19:01:53 -0700

    distributions

--- src/distributions.jl
+++ src/distributions.jl
@@ -4,7 +4,7 @@ export Dists
 const Dists = Distributions
 
 function Measure(dist::Dists.Distribution{F,S}) where {F,S}
-    X = eltype(dist)
+    X = Base.eltype(dist)
     DistributionMeasure{F,S,X}(dist)
 end
 
@@ -27,3 +27,14 @@ export rand
 function Base.rand(μ::DistributionMeasure{F,S,X}) where {F, S, X}
     return rand(μ.dist)
 end
+
+export Normal
+
+function Normal(μ::T, σ::T) where {T <: AbstractFloat}
+    d = Dists.Normal(μ,σ; check_args=false)
+    return Measure(d)
+end
+
+Normal(μ::Real, σ::Real) = Normal(promote(μ, σ)...)
+Normal(μ::Integer, σ::Integer) = Normal(float(μ), float(σ))
+Normal(μ::T) where {T <: Real} = Normal(μ, one(T))
