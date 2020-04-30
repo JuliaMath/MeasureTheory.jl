c8298831a24c7dd93a525cc840ea560a73f846ff
Chad Scherrer <chad.scherrer@gmail.com>
2020-04-29 19:01:43 -0700

    superpose

--- src/combinators/superpose.jl
+++ src/combinators/superpose.jl
@@ -8,9 +8,28 @@ SuperpositionMeasure(ms :: Measure{X}...) where {X} = SuperpositionMeasure{X,len
 
 # SuperpositionMeasure(m::NTuple{N, Measure{X}}) where {N,X} = SuperpositionMeasure(m...)
 
-Base.length(::SuperpositionMeasure{X,N}) where {X,N} = N
+# Base.length(::SuperpositionMeasure{X,N}) where {X,N} = N
 
-function +(μ::SuperpositionMeasure{X,N1}, ν::SuperpositionMeasure{X,N2}) where {X,N1,N2}
-    components = append!!(μ.components, ν.components)
+function Base.:+(μ::SuperpositionMeasure{X,N1}, ν::SuperpositionMeasure{X,N2}) where {X,N1,N2}
+    components = (μ.components..., ν.components...)
     SuperpositionMeasure{X, N1+N2}(components)
 end
+
+function Base.:+(μ::Measure{X}, ν::SuperpositionMeasure{X,N}) where {X,N}
+    components = (μ, ν.components...)
+    SuperpositionMeasure{X,N+1}(components)
+end
+
+function Base.:+(μ::SuperpositionMeasure{X,N}, ν::Measure{X}) where {X,N}
+    components = (μ.components..., ν)
+    SuperpositionMeasure{X,N+1}(components)
+end
+
+function Base.:+(μ::Measure{X}, ν::Measure{X}) where {X}
+    components = (μ, ν)
+    SuperpositionMeasure{X,2}(components)
+end
+
+# TODO : rand(::SuperpositionMeasure)
+# function Base.rand(μ::SuperpositionMeasure{X,N}) where {X,N}
+# end
