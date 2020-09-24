525e757aeaf0b4479356e54ce40a1218a5bf1a8d
Chad Scherrer <chad.scherrer@gmail.com>
2020-09-24 12:53:03 -0700

    more methods for ScaledMeasure

--- src/combinators/scale.jl
+++ src/combinators/scale.jl
@@ -15,6 +15,15 @@ function logdensity(sm::ScaledMeasure{R,M,X}, x::X) where {X, R <: Real, M <: Ab
     logdensity(sm.base, x) + sm.logscale
 end
 
-Base.:*(k::Number, m::AbstractMeasure{X}) where {X} = ScaledMeasure{typeof(k), typeof(m),X}(log(k),m)
+function Base.:*(k::Number, m::AbstractMeasure{X}) where {X} 
+    if iszero(k)
+        return TrivialMeasure(X)
+    else
+        return ScaledMeasure{typeof(k), typeof(m),X}(log(k),m)
+    end
+end
 
 Base.:*(m::AbstractMeasure{X}, k::Real) where {X} = k * m
+
+:≪(::M, ::ScaledMeasure{R,M,X}) where {R,M,X} = true
+:≪(::ScaledMeasure{R,M,X}, ::M) where {R,M,X} = true
