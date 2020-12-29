8570042ac3a3a43ea3d239a2339ca5368251da45
Chad Scherrer <chad.scherrer@gmail.com>
2020-12-29 10:51:41 -0800

    bugfixes

--- src/probability/normal.jl
+++ src/probability/normal.jl
@@ -10,25 +10,25 @@ import Base: eltype
 @measure Normal(μ,σ) ≃ (1/sqrt2π) * Lebesgue(Real)
 
 
-function logdensity(d::Normal{P} , x::X) where {P <: NamedTuple{(:μ, :σ)}, X}    
+function logdensity(d::Normal{(:μ, :σ)} , x::X) where {X}    
     return - log(d.σ)  - (x - d.μ)^2 / (2 * d.σ^2)
 end
 
-function Base.rand(rng::Random.AbstractRNG, d::Normal{P}) where {P <: NamedTuple{(:μ, :σ)}, X}   
+function Base.rand(rng::Random.AbstractRNG, d::Normal{(:μ, :σ)})   
     return randn(rng) * d.σ + d.μ
 end
 
 # Standard normal
 
-function logdensity(d::Normal{EmptyNamedTuple} , x::X) where {X}  
+function logdensity(d::Normal{()} , x::X) where {X}  
     return - x^2 / 2 
 end
 
 # Normal() = Normal{EmptyNamedTuple,Real}(NamedTuple())
  
-sampletype(::Normal{NamedTuple{(),Tuple{}}}) = Real
+sampletype(::Normal{()}) = Real
 
-Base.rand(rng::Random.AbstractRNG, μ::Normal{EmptyNamedTuple}) = randn(rng)
+Base.rand(rng::Random.AbstractRNG, μ::Normal{()}) = randn(rng)
 
 ≪(::Normal, ::Lebesgue{X}) where X <: Real = true
 representative(::Normal) = Lebesgue(Real)
