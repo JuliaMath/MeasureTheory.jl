05fe9d0960221651609d2d731bd95f9cc5381da6
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-13 08:12:27 -0800

    updates to Normal

--- src/probability/normal.jl
+++ src/probability/normal.jl
@@ -9,16 +9,6 @@ import Base: eltype
 
 @measure Normal(μ,σ) ≃ (1/sqrt2π) * Lebesgue(ℝ)
 
-
-function logdensity(d::Normal{(:μ, :σ)} , x::X) where {X} 
-    z = (x - d.μ) / d.σ   
-    return - log(d.σ)  - logdensity(Normal(), z)
-end
-
-function Base.rand(rng::Random.AbstractRNG, d::Normal{(:μ, :σ)})   
-    return randn(rng) * d.σ + d.μ
-end
-
 # Standard normal
 
 function logdensity(d::Normal{()} , x::X) where {X}  
@@ -33,3 +23,5 @@ Base.rand(rng::Random.AbstractRNG, μ::Normal{()}) = randn(rng)
 
 ≪(::Normal, ::Lebesgue{X}) where X <: Real = true
 representative(::Normal) = Lebesgue(ℝ)
+
+@μσ_methods Normal()
