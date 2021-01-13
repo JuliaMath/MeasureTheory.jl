c04dcf38fc1bf900922eca31f7ee6c4c0858a6e1
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-13 12:12:20 -0800

    Gumbel distribution

--- src/probability/gumbel.jl
+++ src/probability/gumbel.jl
@@ -5,13 +5,20 @@ export Gumbel
 
 @measure Gumbel(μ,σ) ≃ Lebesgue(ℝ)
 
-function logdensity(d::Gumbel{EmptyNamedTuple} , x::MaybeSym{T}) where {T <: Number}  
+function logdensity(d::Gumbel{()} , x::MaybeSym{T}) where {T <: Number}  
     return -exp(-x) - x
 end
  
-sampletype(::Gumbel{NamedTuple{(),Tuple{}}}) = Real
+sampletype(::Gumbel{()}) = Real
 
-Base.rand(μ::Gumbel{EmptyNamedTuple}) = rand(Dists.Gumbel())
+import Base
+
+function Base.rand(rng::AbstractRNG, d::Gumbel{()})
+    u = rand()
+    log(-log(u))
+end
 
 ≪(::Gumbel, ::Lebesgue{X}) where X <: Real = true
 representative(::Gumbel) = Lebesgue(ℝ)
+
+@μσ_methods Gumbel()
