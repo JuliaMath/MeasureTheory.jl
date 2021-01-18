437976c8b0af00aac5d77b23abdc924f0f607772
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-18 09:06:01 -0800

    oops missed a push

--- src/utils.jl
+++ src/utils.jl
@@ -2,7 +2,3 @@ const EmptyNamedTuple = NamedTuple{(),Tuple{}}
 
 showparams(io::IO, ::EmptyNamedTuple) = print(io, "()")
 showparams(io::IO, nt::NamedTuple) = print(io, nt)
-
-using SymbolicUtils: Symbolic
-
-const MaybeSym{T} = Union{T, Symbolic{T}}
