b6dcb66834cba43cc81dc4920211f75218412a73
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-18 14:16:52 -0800

    exponential

--- src/probability/exponential.jl
+++ src/probability/exponential.jl
@@ -12,8 +12,38 @@ end
 
 sampletype(::Exponential) = Real
 
-Base.rand(μ::Exponential{()}) = -log(rand())
+Base.rand(rng::AbstractRNG, μ::Exponential{()}) = -log(rand(rng))
 
 ≪(::Exponential, ::Lebesgue{ℝ₊}) = true
 
 representative(::Exponential) = Lebesgue(ℝ₊)
+
+
+##########################
+
+function Base.rand(rng::AbstractRNG, d::Exponential{(:min, :λ)})
+    rand(rng, Exponential()) / d.λ + d.min
+end
+
+function logdensity(d::Exponential{(:min, :λ)}, x)
+    z = (x - d.min) * d.λ
+    return logdensity(Exponential(), z) + log(d.λ)
+end
+
+function Base.rand(rng::AbstractRNG, d::Exponential{(:λ,)})
+    rand(rng, Exponential()) / d.λ
+end
+
+function logdensity(d::Exponential{(:λ,)}, x)
+    z = x * d.λ
+    return logdensity(Exponential(), z) + log(d.λ)
+end
+
+function Base.rand(rng::AbstractRNG, d::Exponential{(:min,)})
+    rand(rng, Exponential()) + d.min
+end
+
+function logdensity(d::Exponential{(:min,)}, x)
+    z = x - d.min
+    return logdensity(Exponential(), z)
+end
