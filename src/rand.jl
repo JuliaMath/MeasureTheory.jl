e3151735d96b6f6897c943affe4f2c722d24cd9b
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-20 17:05:06 -0800

    include type in `rand` dispatch

--- src/rand.jl
+++ src/rand.jl
@@ -1,1 +1,10 @@
-Base.rand(μ::AbstractMeasure) = rand(Random.GLOBAL_RNG, μ)
+Base.rand(μ::AbstractMeasure) = rand(Random.GLOBAL_RNG, sampletype(μ), μ)
+
+# struct ArraySlot{A,I}
+#     arr::A
+#     i::I
+# end
+
+# function rand!(rng::AbstractRNG, d::AbstractMeasure, x::ArraySlot)
+#     x.arr[x.i...] = rand(rng, d)
+# end
