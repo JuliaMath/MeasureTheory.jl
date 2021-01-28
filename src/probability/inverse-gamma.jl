57d135f5e276ac8c4229a986e835554761604ba0
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-28 10:31:37 -0800

    invserse gamma

---
+++ src/probability/inverse-gamma.jl
@@ -0,0 +1,22 @@
+
+# InverseGamma distribution
+
+using SpecialFunctions
+using StatsFuns
+export InverseGamma
+
+@measure InverseGamma(shape) ≃ Lebesgue(ℝ₊)
+
+function logdensity(μ::InverseGamma{(:shape,)}, x) 
+    α = μ.shape
+    xinv = 1/x
+
+    return (α + 1) * log(xinv) - xinv - loggamma(α) 
+end
+
+Base.rand(rng::AbstractRNG, T::Type, μ::InverseGamma{(:shape,)}) = rand(rng, Dists.InverseGamma(μ.shape))
+
+≪(::InverseGamma, ::Lebesgue{X}) where X <: Real = true
+representative(::InverseGamma) = Lebesgue(ℝ₊)
+
+@μσ_methods InverseGamma(shape)
