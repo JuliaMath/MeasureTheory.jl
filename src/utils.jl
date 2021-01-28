ec1c1bd3ed44ed773ce4a70fda316467c2f290c7
Chad Scherrer <chad.scherrer@gmail.com>
2021-01-28 15:53:27 -0800

    `fix`

--- src/utils.jl
+++ src/utils.jl
@@ -2,3 +2,13 @@ const EmptyNamedTuple = NamedTuple{(),Tuple{}}
 
 showparams(io::IO, ::EmptyNamedTuple) = print(io, "()")
 showparams(io::IO, nt::NamedTuple) = print(io, nt)
+
+function fix(f, x)
+    y = f(x)
+
+    if y == x 
+        return y
+    else
+        return fix(f, y)
+    end
+end
