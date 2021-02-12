82d3dc725ad70408dd52133b50bf6c0a2d0b6bcd
Chad Scherrer <chad.scherrer@gmail.com>
2021-02-11 16:06:52 -0800

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,34 @@
 using MeasureTheory
 using Test
 
-@testset "MeasureTheory.jl" begin
-    # Write your own tests here.
+function draw2(μ)
+    x = rand(μ)
+    y = rand(μ)
+    while x == y
+        y = rand(μ)
+    end
+    return (x,y)
+end
+
+using Test
+using StatsFuns
+
+@testset "Parameterized Measures" begin
+
+    @testset "Binomial" begin
+        (n,p) = (10,0.3)
+        logit_p = logit(p)
+        (x,y) = draw2(Binomial(n,p))
+
+        let Δlogπ(n,p,x) = logdensity(Binomial(n,p), x) - binomlogpdf(n,p,x)
+            @test Δlogπ(n,p,x) ≈ Δlogπ(n,p,y)
+        end
+
+        @test logdensity(Binomial(;n,p), x) ≈ logdensity(Binomial(; n, logit_p), x)
+
+        @test_broken logdensity(Binomial(n,p), CountingMeasure(ℤ[0:n]), x) ≈ binomlogpdf(n,p,x)
+    end
+
 end
 
 @testset "Kernel" begin
