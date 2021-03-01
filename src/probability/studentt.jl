27f5b5bc6812486ef90b74b978ae97181a0f9e02
Chad Scherrer <chad.scherrer@gmail.com>
2021-03-01 13:53:47 -0800

    distproxy for student T

--- src/probability/studentt.jl
+++ src/probability/studentt.jl
@@ -18,5 +18,7 @@ Base.rand(rng::AbstractRNG, T::Type, μ::StudentT{(:ν,)}) = rand(rng, Dists.TDi
 ≪(::StudentT, ::Lebesgue{X}) where X <: Real = true
 representative(::StudentT) = Lebesgue(ℝ)
 
+distproxy(d::StudentT{(:ν, :μ, :σ)}) = Dists.LocationScale(d.μ, d.σ, Dists.TDist(d.ν))
+
 @μσ_methods StudentT(ν)
 @half StudentT()
