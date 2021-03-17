c1a5868bbdf96bc13d67ba9be7b962996f7e92d2
Moritz Schauer <moritzschauer@web.de>
2021-03-17 15:53:27 +0100

    Requested reference (#76)

--- src/combinators/spikemixture.jl
+++ src/combinators/spikemixture.jl
@@ -13,6 +13,8 @@ function Base.show(io::IO, μ::SpikeMixture)
 end
 
 function basemeasure(μ::SpikeMixture)
+    # Compare formula (1.4) in Joris Bierkens, Sebastiano Grazzi, Frank van der Meulen, Moritz Schauer:
+    # Sticky PDMP samplers for sparse and local inference problems. 2020. [https://arxiv.org/abs/2103.08478].
     ki = (1/μ.w - 1)/density(μ.m, 0)
     SpikeMixture(basemeasure(μ.m), 1/(1+ki), μ.s*(1+ki))
 end
