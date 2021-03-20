b9ec4d51606129ca1e4071c60aa71d59e0c3a788
Chad Scherrer <chad.scherrer@gmail.com>
2021-03-20 07:59:09 -0700

    revert base measure for now

--- src/probability/normal.jl
+++ src/probability/normal.jl
@@ -8,11 +8,6 @@ export Normal
 
 basemeasure(::Normal) = (1/sqrt2π) * Lebesgue(ℝ)
 
-
-basemeasure(d::Normal{(:μ,:σ)}) = ifelse(iszero(d.σ), Dirac(d.μ), (1/sqrt2π) * Lebesgue(ℝ))
-
-basemeasure(d::Normal{(:σ,)}) = ifelse(iszero(d.σ), Dirac(0.0), (1/sqrt2π) * Lebesgue(ℝ))
-
 logdensity(d::Normal{()} , x) = - x^2 / 2 
 
 Base.rand(rng::Random.AbstractRNG, T::Type, μ::Normal{()}) = randn(rng, T)
