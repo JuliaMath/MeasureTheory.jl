27fca155aa68b72b12915512abd627333025828a
Chad Scherrer <chad.scherrer@gmail.com>
2021-03-22 16:03:38 -0700

    more normal methods

--- src/probability/normal.jl
+++ src/probability/normal.jl
@@ -18,6 +18,47 @@ Base.rand(rng::Random.AbstractRNG, T::Type, μ::Normal{()}) = randn(rng, T)
 
 HalfNormal(σ) = HalfNormal(σ = σ)
 
+
+###############################################################################
+# (μ,σ²)
+
+function logdensity(d::Normal{(:σ²)}, x)
+    σ² = d.σ²
+    -0.5 * (log(σ²) + (x^2/σ²))
+end
+
+function logdensity(d::Normal{(:μ,:σ²)}, x)
+    μ = d.μ
+    σ² = d.σ²
+    -0.5 * (log(σ²) + ((x - μ)^2/σ²))
+end
+
+###############################################################################
+# (μ,τ)
+
+function logdensity(d::Normal{(:τ)}, x)
+    τ = d.τ
+    0.5 * (log(τ) - τ * x^2)
+end
+
+function logdensity(d::Normal{(:μ,:τ)}, x)
+    μ = d.μ
+    τ = d.τ
+    0.5 * (log(τ) - τ * (x - μ)^2)
+end
+
+
+###############################################################################
+# (μ,logσ)
+
+function logdensity(d::Normal{(:μ,:logσ)}, x)
+    μ = d.μ
+    logσ = d.logσ
+    -logσ - 0.5(exp(-2logσ)*((x - μ)^2))
+end
+
+
+
 distproxy(d::Normal{(:μ, :σ)}) = Dists.Normal(d.μ, d.σ)
 
 asparams(::Type{<:Normal}, ::Val{:μ}) = asℝ
