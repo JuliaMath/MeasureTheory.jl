ce09f7f019f178450efae23220b04a4c2bedeb5a
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-25 09:55:34 -0700

    Move counting.jl to primitives

--- src/primitives/counting.jl
+++ src/primitives/counting.jl
@@ -2,16 +2,14 @@ export CountingMeasure
 
 struct CountingMeasure{X} <: AbstractMeasure end
 
+isprimtype(::CountingMeasure) = true
+
 function Base.show(io::IO, μ::CountingMeasure{X}) where {X}
     io = IOContext(io, :compact => true)
     print(io, "CountingMeasure(", X, ")")
 end
 
 CountingMeasure(X) = CountingMeasure{X}()
-
-basemeasure(μ::CountingMeasure) = μ
-
-isprimitive(::CountingMeasure) = true
 
 # sampletype(::CountingMeasure{ℝ}) = Float64
 # sampletype(::CountingMeasure{ℝ₊}) = Float64
