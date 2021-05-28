af62df889a55c361ed7cd01521136de303f50214
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-28 10:09:20 -0700

    update Cauchy

--- src/parameterized/cauchy.jl
+++ src/parameterized/cauchy.jl
@@ -6,6 +6,11 @@ export Cauchy
 
 @parameterized Cauchy(μ,σ) ≪ (1/π) * Lebesgue(ℝ)
 
+@kwstruct Cauchy()
+
+@μσ_methods Cauchy()
+@kwstruct Cauchy(μ,σ)
+
 function logdensity(d::Cauchy{()} , x) 
     return -log(1 + x^2)
 end
@@ -15,7 +20,7 @@ Base.rand(rng::AbstractRNG, T::Type, μ::Cauchy{()}) = randn(rng, T) / randn(rng
 ≪(::Cauchy, ::Lebesgue{X}) where X <: Real = true
 representative(::Cauchy) = Lebesgue(ℝ)
 
-@μσ_methods Cauchy()
+
 @half Cauchy()
 
 distproxy(d::Cauchy{()}) = Dists.Cauchy()
