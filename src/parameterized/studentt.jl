4ea8247b6beb8ac641ef58b146f0abe2427fb39c
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-28 09:47:34 -0700

    StudentT

--- src/parameterized/studentt.jl
+++ src/parameterized/studentt.jl
@@ -7,6 +7,21 @@ export StudentT
 
 @parameterized StudentT(ν) ≪ (1/sqrtπ) * Lebesgue(ℝ)
 
+@kwstruct StudentT(ν)
+
+@kwstruct StrudentT(ν, μ, σ)
+StudentT(ν, μ, σ) = StudentT((ν=ν, μ=μ, σ=σ))
+
+@kwalias StudentT [
+    df       => ν
+    nu       => ν
+    location => μ
+    mean     => μ # Doesn't really always exist, but ok fine
+    mu       => μ
+    scale    => σ
+    sigma    => σ
+]
+
 function logdensity(d::StudentT{(:ν,)}, x) 
     ν = d.ν
     halfνp1 = (ν+1)/2
@@ -21,4 +36,4 @@ representative(::StudentT) = Lebesgue(ℝ)
 distproxy(d::StudentT{(:ν, :μ, :σ)}) = Dists.LocationScale(d.μ, d.σ, Dists.TDist(d.ν))
 
 @μσ_methods StudentT(ν)
-@half StudentT()
+@half StudentT(ν)
