04f561aa766aa8945cd40489bb364bd7891da8c0
Chad Scherrer <chad.scherrer@gmail.com>
2021-06-02 07:24:34 -0700

    typo

--- src/transforms/corrcholeskylower.jl
+++ src/transforms/corrcholeskylower.jl
@@ -9,7 +9,7 @@ using LinearAlgebra
 
 """
     CorrCholeskyLower(n)
-Cholesky Lower of a correlation matrix of size `n`.
+Lower Cholesky factor of a correlation matrix of size `n`.
 Transforms ``n(n-1)/2`` real numbers to an ``n×n`` lower-triangular matrix `L`, such that
 `L*L'` is a correlation matrix (positive definite, with unit diagonal).
 
