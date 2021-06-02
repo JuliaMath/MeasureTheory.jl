1c261e5cd957be130f6d7537116ea2a99516fd59
Chad Scherrer <chad.scherrer@gmail.com>
2021-06-01 19:09:48 -0700

    add some constructors

--- src/parameterized/LKJL.jl
+++ src/parameterized/LKJL.jl
@@ -7,11 +7,11 @@ export LKJL
 The LKJ distribution (Lewandowski et al 2009) for the Cholesky factor L of correlation
 matrices.
 
-A correlation matrix ``Ω=LL'`` has the density ``|Ω|^{η-1}``. However, it is usually not
+A correlation matrix ``Ω=L*L'`` has the density ``|Ω|^{η-1}``. However, it is usually not
 necessary to construct ``Ω``, so this distribution is formulated for the Cholesky
 decomposition `L*L'`, and takes `L` directly.
 
-Note that the methods **does not check if `L` yields a valid correlation matrix**.
+Note that the method **does not check if `L` yields a valid correlation matrix**.
 Valid values are ``η > 0``. When ``η > 1``, the distribution is unimodal at `Ω=I`, while
 ``0 < η < 1`` has a trough. ``η = 2`` is recommended as a vague prior.
 When ``η = 1``, the density is uniform in `Ω`, but not in `L`, because of the Jacobian
@@ -24,7 +24,17 @@ struct LKJL{k, N, T} <: ParameterizedMeasure{N}
     par :: NamedTuple{N,T}
 end
 
+LKJL{k}(nt::NamedTuple{N,T}) where {k,N,T} = LKJL{k,N,T}(nt)
+
 LKJL(k,η) = LKJL{k, (:η,), Tuple{typeof(η)}}((η=η,))
+
+LKJL(k) = LKJL(k, 1.0)
+
+LKJL{k}(η) where {k} = LKJL{k, (:η,), Tuple{typeof(η)}}((η=η,))
+
+function Base.show(io::IO, d::LKJL{k}) where {k}
+    println(io, "LKJL{", k, "}(", getfield(d, :par), ")")
+end
 
 # Modified from
 # https://github.com/tpapp/AltDistributions.jl
