84791e7502aa9a8ae449a1ff2e5868088e232d53
Chad Scherrer <chad.scherrer@gmail.com>
2021-06-09 14:56:21 -0700

    docstring

--- src/combinators/likelihood.jl
+++ src/combinators/likelihood.jl
@@ -1,8 +1,44 @@
 export LogLikelihood
 
-@concrete terse struct LogLikelihood
-    f
-    x
+"""
+    LogLikelihood(M<:ParameterizedMeasure, x)
+
+"Observe" a value `x`, yielding a function from the parameters to ℝ.
+
+EXAMPLE:
+    julia> LogLikelihood(Normal, 3)(μ=2)
+    -0.5
+
+If `M` has a name, we don't need one in the argument.
+
+    julia> LogLikelihood(Normal{(:μ,)}, 3)(2)
+    -0.5
+
+---------
+
+    LogLikelihood(M<:ParameterizedMeasure, constraint::NamedTuple, x)
+
+As above, but merge the new values with `constraint` before evaluation
+
+    julia> LogLikelihood(Normal, (μ=2,), 5)((σ=1,))
+    -4.5
+
+We can again provide named in the first argument:
+
+    julia> LogLikelihood(Normal{(:μ,:σ)}, (μ=2,), 5)(1)
+    -4.5
+"""
+struct LogLikelihood{F,X}
+    f::F
+    x::X
+end
+
+function show(io::IO, ℓ::LogLikelihood{Tuple{M,NT}}) where {M<:Type, NT <: NamedTuple}
+    println(io, "LogLikelihood(",ℓ.f..., ℓ.x, ")")
+end
+
+function show(io::IO, ℓ::LogLikelihood)
+    println(io, "LogLikelihood(",ℓ.f, ℓ.x, ")")
 end
 
 LogLikelihood(μ::M, x) where {M<:AbstractMeasure} = LogLikelihood(M, x)
