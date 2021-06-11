beff4c6405860fb37c209dd820945c8feaf04d92
Chad Scherrer <chad.scherrer@gmail.com>
2021-06-11 09:10:11 -0700

    `rootmeasure` docstring

--- src/utils.jl
+++ src/utils.jl
@@ -43,4 +43,11 @@ testvalue(μ::AbstractMeasure) = testvalue(basemeasure(μ))
 
 export rootmeasure
 
+"""
+    rootmeasure(μ::AbstractMeasure)
+
+It's sometimes important to be able to find the fix point of a measure under
+`basemeasure`. That is, to start with some measure and apply `basemeasure`
+repeatedly until there's no change. That's what this does.
+"""
 rootmeasure(μ::AbstractMeasure) = fix(basemeasure, μ)
