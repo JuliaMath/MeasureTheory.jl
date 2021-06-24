6563ed4d8462283e59fd499cc20fbee0cbcd38c7
Chad Scherrer <chad.scherrer@gmail.com>
2021-06-23 19:06:12 -0700

    updates to kernel

--- src/kernel.jl
+++ src/kernel.jl
@@ -1,8 +1,12 @@
+using KeywordCalls: instance_type
+
+
 export Kernel
 
 abstract type AbstractKernel <: AbstractMeasure end
 
-struct Kernel{T,S} <: AbstractKernel
+struct Kernel{F,S} <: AbstractKernel
+    f::F
     ops::S
 end
 
@@ -25,16 +29,62 @@ If the argument is a named tuple `(;a=f1, b=f1)`, `κ(x)` is defined as
 function kernel end
 
 export kernel
-kernel(op, ::Type{M}) where {M} = Kernel{M,typeof(op)}(op)
-kernel(::Type{M}; ops...) where {M} = Kernel{M,typeof(ops.data)}(ops.data)
+
+kernel(μ, ops...) = Kernel(μ, ops)
+kernel(μ, op) = Kernel(μ, op)
+
+# kernel(Normal(μ=2))
+function kernel(μ::P) where {P <: AbstractMeasure}
+    (f, ops) = _kernelfactor(μ)
+    Kernel{instance_type(f), typeof(ops)}(f,ops)
+end
+
+# kernel(Normal{(:μ,), Tuple{Int64}})
+function kernel(::Type{P}) where {P <: AbstractMeasure}
+    (f, ops) = _kernelfactor(P)
+    Kernel{instance_type(f), typeof(ops)}(f,ops)
+end
+
+
+
+# kernel(::Type{P}, op::O) where {O, N, P<:ParameterizedMeasure{N}} = Kernel{constructor(P),O}(op)
+
+# kernel(::Type{M}; ops...) where {M} = Kernel{Type{M},typeof(ops.data)}(M,ops.data)
+
+# TODO: Would this benefit from https://github.com/tisztamo/FunctionWranglers.jl?
 mapcall(t, x) = map(func -> func(x), t)
-(k::Kernel{M,<:Tuple})(x) where {M} = M(mapcall(k.ops, x)...)
-(k::Kernel{M,<:NamedTuple})(x) where {M} = M(;mapcall(k.ops, x)...)
-(k::Kernel{M})(x) where {M} = M(k.ops(x)...)
 
-export kernelize
+# (k::Kernel{Type{P},<:Tuple})(x) where {P<:ParameterizedMeasure} = k.f(mapcall(k.ops, x)...)
 
-function kernelize(μ::M) where {N, M <: ParameterizedMeasure{N}}
-    C = M.name.wrapper
-    (kernel(NamedTuple{N}, C), values(getfield(μ, :par)))
+# (k::Kernel{M,<:NamedTuple})(x) where {M} = k.f(;mapcall(k.ops, x)...)
+
+# (k::Kernel)(x) = k.f(k.ops(x)...)
+
+(k::Kernel{F,S})(x...) where {F, N, S<:NTuple{N,Symbol}} = k(x)
+
+
+function (k::Kernel{F,S})(x::Tuple) where {F, N, S<:NTuple{N,Symbol}}
+    k.f(NamedTuple{k.ops}(x))
+end
+
+
+
+# export kernelize
+
+# function kernelize(μ::M) where {N, M <: ParameterizedMeasure{N}}
+#     C = constructor(M)
+#     (Kernel{C,}(NamedTuple{N}, ), values(getfield(μ, :par)))
+# end
+
+function _kernelfactor(::Type{P}) where {N, P <: ParameterizedMeasure{N}}
+    (constructor(P), N)
+end
+
+function _kernelfactor(::P) where {N, P <: ParameterizedMeasure{N}}
+    (constructor(P), N)
+end
+
+function _kernelfactor(μ::P) where {N, D<:ParameterizedMeasure{N}, P <: PowerMeasure{D}}
+    C = constructor(D)
+    (p -> C(p) ^ size(μ.data), N)
 end
