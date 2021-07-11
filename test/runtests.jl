270f8bb5de12e26003e0a5cbaf38c4b1750cc89c
Chad Scherrer <chad.scherrer@gmail.com>
2021-07-11 15:21:36 -0700

    update LKJ test

--- test/runtests.jl
+++ test/runtests.jl
@@ -78,18 +78,18 @@ end
     end
 
     @testset "LKJCholesky" begin
-        D = LKJCholesky{4}{(:η,)}
-        par = transform(asparams(D), randn(1))
-        d = D(par)
-        @test params(d) == par
+        D = LKJCholesky{(:k,:η)}
+        par = transform(asparams(D, (k=4,)), randn(1))
+        d = D(merge((k=4,),par))
+        # @test params(d) == par
 
         η  = par.η
         logη = log(η)
 
         y = rand(d)
         η = par.η
-        ℓ = logdensity(LKJCholesky{4}(η), y)
-        @test ℓ ≈ logdensity(LKJCholesky{4}(logη=logη), y)
+        ℓ = logdensity(LKJCholesky(4,η), y)
+        @test ℓ ≈ logdensity(LKJCholesky(k=4,logη=logη), y)
     end
 end
 
