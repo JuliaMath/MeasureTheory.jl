6a8b3f8ace6fa6c4a50f3fbc99da809742bde9f6
Chad Scherrer <chad.scherrer@gmail.com>
2021-07-13 07:28:19 -0700

    code organization

--- src/parameterized/negativebinomial.jl
+++ src/parameterized/negativebinomial.jl
@@ -21,6 +21,8 @@ function logdensity(d::NegativeBinomial{(:r, :p)}, y)
     return -log(y + r) - logbeta(r, y+1) + y * log(p) + r * log1p(-p)
 end
 
+distproxy(d::NegativeBinomial{(:r, :p)}) = Dists.NegativeBinomial(d.r, d.p)
+
 ###############################################################################
 @kwstruct NegativeBinomial(r, logitp)
 
@@ -28,6 +30,8 @@ function logdensity(d::NegativeBinomial{(:r, :logitp)}, y)
     (r, logitp) = (d.r, d.logitp)
     return -log(y + r) - logbeta(r, y+1) - y * log1pexp(-logitp) - r * log1pexp(logitp)
 end
+
+distproxy(d::NegativeBinomial{(:n,:logitp)}) = Dists.NegativeBinomial(d.n, logistic(d.logitp))
 
 ###############################################################################
 @kwstruct NegativeBinomial(r, λ)
@@ -39,6 +43,19 @@ function logdensity(d::NegativeBinomial{(:r, :λ)}, y)
     return -log(y + r) - logbeta(r, y+1) + y * log(λ) + r * log(r) - (y + r) * log(r + λ)
 end
 
+function distproxy(d::NegativeBinomial{(:r,:λ)})
+    p = d.λ / (d.r + d.λ)
+    return Dists.NegativeBinomial(d.r, p)
+end
+
+# # https://en.wikipedia.org/wiki/Negative_binomial_distribution#Gamma%E2%80%93Poisson_mixture
+# function Base.rand(rng::AbstractRNG, d::NegativeBinomial{(:r,:λ)})
+#     r = d.r
+#     λ = d.λ
+#     μ = rand(rng, Dists.Gamma(r, λ/r))
+#     return rand(rng, Dists.Poisson(μ))
+# end
+
 ###############################################################################
 @kwstruct NegativeBinomial(r, logλ)
 
@@ -48,23 +65,15 @@ function logdensity(d::NegativeBinomial{(:r, :logλ)}, y)
     return -log(y + r) - logbeta(r, y+1) + y * logλ + r * log(r) - (y + r) * log(r + λ)
 end
 
-distproxy(d::NegativeBinomial{(:r, :p)}) = Dists.NegativeBinomial(d.r, d.p)
-distproxy(d::NegativeBinomial{(:n,:logitp)}) = Dists.NegativeBinomial(d.n, logistic(d.logitp))
-
-function distproxy(d::NegativeBinomial{(:r,:λ)})
-    p = d.λ / (d.r + d.λ)
+function distproxy(d::NegativeBinomial{(:r,:logλ)})
+    λ = exp(d.logλ)
+    p = λ / (d.r + λ)
     return Dists.NegativeBinomial(d.r, p)
 end
 
-# https://en.wikipedia.org/wiki/Negative_binomial_distribution#Gamma%E2%80%93Poisson_mixture
-function Base.rand(rng::AbstractRNG, d::NegativeBinomial{(:r,:λ)})
-    r = d.r
-    λ = d.λ
-    μ = rand(rng, Dists.Gamma(r, λ/r))
-    return rand(rng, Dists.Poisson(μ))
-end
-
 Base.rand(rng::AbstractRNG, d::NegativeBinomial{(:r,:logλ)}) = rand(rng, NegativeBinomial{(:r, :λ)}(d.r, exp(d.logλ)))
+
+###############################################################################
 
 asparams(::Type{<:NegativeBinomial}, ::Val{:p}) = as𝕀
 asparams(::Type{<:NegativeBinomial}, ::Val{:logitp}) = asℝ
