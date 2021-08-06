7344016b552c5093da6dbbcd4ad0dcfbd248097d
Chad Scherrer <chad.scherrer@gmail.com>
2021-08-06 16:03:12 -0700

    moved CorrCholesky test

--- test/runtests.jl
+++ test/runtests.jl
@@ -85,20 +85,6 @@ end
         @test ℓ ≈ logdensity(Normal(;μ,logσ), y)
     end
 
-    @testset "LKJCholesky" begin
-        D = LKJCholesky{(:k,:η)}
-        par = transform(asparams(D, (k=4,)), randn(1))
-        d = D(merge((k=4,),par))
-        # @test params(d) == par
-
-        η  = par.η
-        logη = log(η)
-
-        y = rand(d)
-        η = par.η
-        ℓ = logdensity(LKJCholesky(4,η), y)
-        @test ℓ ≈ logdensity(LKJCholesky(k=4,logη=logη), y)
-    end
 end
 
 @testset "Kernel" begin
