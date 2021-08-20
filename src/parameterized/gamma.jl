2e7bc929cc3794bff3351cb240fc1c58658a5b8b
Chad Scherrer <chad.scherrer@gmail.com>
2021-08-20 13:36:53 -0700

    Revert "include gamma.jl"
    This reverts commit 1d511229ef0ed97e8704c427846f5ddb10ad78c1.

--- src/parameterized/gamma.jl
+++ src/parameterized/gamma.jl
@@ -19,7 +19,7 @@ function logdensity(d::Gamma{(:α, :β)}, x)
     return d.α * log(d.β) + (d.α - 1) * log(x) - d.β * x - loggamma(d.α)
 end
 
-# Base.rand(rng::AbstractRNG, T::Type, μ::Beta) = rand(rng, Dists.Gamma(μ.α, 1/μ.β))
+Base.rand(rng::AbstractRNG, T::Type, μ::Beta) = rand(rng, Dists.Gamma(μ.α, 1/μ.β))
 
 distproxy(d::Gamma{(:α, :β)}) = Dists.Gamma(d.α, 1/d.β)
 
