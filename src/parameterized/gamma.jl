5d21765a2426a9c13828acd30f5e6eded0928f38
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2021-08-20 20:38:06 +0200

    Define Gamma parametrized measure

---
+++ src/parameterized/gamma.jl
@@ -0,0 +1,27 @@
+# Gamma distribution
+
+export Gamma
+
+@parameterized Gamma(α,β) ≪ Lebesgue(ℝ₊)
+
+@kwstruct Gamma(α, β)
+
+@kwalias Gamma [
+    a     => α
+    alpha => α
+    b     => β
+    beta  => β
+]
+
+TV.as(::Gamma) = asℝ₊
+
+function logdensity(d::Gamma{(:α, :β)}, x)
+    return d.α * log(d.β) + (d.α - 1) * log(x) - d.β * x - loggamma(d.α)
+end
+
+Base.rand(rng::AbstractRNG, T::Type, μ::Beta) = rand(rng, Dists.Gamma(μ.α, 1/μ.β))
+
+distproxy(d::Gamma{(:α, :β)}) = Dists.Gamma(d.α, 1/d.β)
+
+asparams(::Type{<:Gamma}, ::Val{:α}) = asℝ₊
+asparams(::Type{<:Gamma}, ::Val{:β}) = asℝ₊
