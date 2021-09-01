90373252c42aa7383a0cfefdd8cbe5706ed1d869
Chad Scherrer <chad.scherrer@gmail.com>
2021-09-01 16:07:49 -0700

    update Uniform

--- src/parameterized/uniform.jl
+++ src/parameterized/uniform.jl
@@ -3,16 +3,28 @@
 
 export Uniform
 
-@parameterized Uniform() ≪ Lebesgue(𝕀)
+@parameterized Uniform()
 @kwstruct Uniform()
 
 
+###############################################################################
 # Standard Uniform
+
+function basemeasure(::Uniform{()})
+    inbounds(x) = 0 < x < 1
+    constℓ = 0.0
+    varℓ = 0.0
+    base = Lebesgue(ℝ)
+    FactoredBase(inbounds, constℓ, varℓ, base)
+end
 
 distproxy(::Uniform{()}) = Dists.Uniform()
 
 logdensity(d::Uniform{()}, x) = 0.0
 
-TV.as(::Uniform) = as𝕀
+TV.as(::Uniform{()}) = as𝕀
 
 Base.rand(rng::AbstractRNG, T::Type, μ::Uniform{()}) = rand(rng, T)
+
+###############################################################################
+# Uniform
