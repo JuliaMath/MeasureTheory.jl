be5be5cdd7878b378393080003a8f1a5d40ef0e4
Chad Scherrer <chad.scherrer@gmail.com>
2021-10-13 11:29:49 -0700

    asparams(::Affine)

--- src/combinators/affine.jl
+++ src/combinators/affine.jl
@@ -3,3 +3,5 @@ asparams(::Affine, ::Val{:σ}) = asℝ₊
 asparams(::Type{A}, ::Val{:μ}) where {A<:Affine} = asℝ
 asparams(::Type{A}, ::Val{:σ}) where {A<:Affine} = asℝ₊
 
+asparams(::Affine{N,M,T}, ::Val{:μ}) where {N,M,T<:AbstractArray} = as(Array, asℝ, size(d.μ))
+asparams(::Affine{N,M,T}, ::Val{:σ}) where {N,M,T<:AbstractArray} = as(Array, asℝ, size(d.σ))
