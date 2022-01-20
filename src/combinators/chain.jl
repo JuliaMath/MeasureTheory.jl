b0c897f003c49d1398fd8861b825159536ce3899
Chad Scherrer <chad.scherrer@gmail.com>
2022-01-20 09:51:49 -0800

    Simplify logdensity_def(::DynamicFor{M}, y) for now

--- src/combinators/chain.jl
+++ src/combinators/chain.jl
@@ -79,7 +79,8 @@ function Base.rand(rng::AbstractRNG, T::Type, df::DynamicFor)
 end
 
 @inline function logdensity_def(df::DynamicFor{M}, y::T) where {M,T}
-    ℓ = zero(float(Core.Compiler.return_type(logdensity_def, Tuple{M,T})))
+    # ℓ = zero(float(Core.Compiler.return_type(logdensity_def, Tuple{M,T})))
+    ℓ = 0.0
     for (xj, yj) in zip(df.iter, y)
         ℓ += logdensity_def(df.κ(xj), yj)
     end
