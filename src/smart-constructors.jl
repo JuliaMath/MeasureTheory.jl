3c7bef3ed805d944e55b6cc0287c35073006271b
Chad Scherrer <chad.scherrer@gmail.com>
2022-05-02 13:42:18 -0700

    drop FactoredBase

--- src/smart-constructors.jl
+++ src/smart-constructors.jl
@@ -11,13 +11,6 @@ function affine(f::AffineTransform, parent::WeightedMeasure)
     WeightedMeasure(parent.logweight, affine(f, parent.base))
 end
 
-function affine(f::AffineTransform, parent::FactoredBase)
-    constℓ = parent.constℓ
-    varℓ = parent.varℓ
-    base = affine(f, parent.base)
-    FactoredBase(constℓ, varℓ, base)
-end
-
 using MeasureBase: RealNumbers
 
 function affine(f::AffineTransform{(:μ, :σ)}, parent::Lebesgue{RealNumbers})
