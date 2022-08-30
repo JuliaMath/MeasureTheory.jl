beb231627d4773ccda672e2699f2d6f400443cee
Chad Scherrer <chad.scherrer@gmail.com>
2022-08-30 13:40:01 -0700

    run JuliaFormatter

--- src/parameterized/betabinomial.jl
+++ src/parameterized/betabinomial.jl
@@ -4,7 +4,6 @@ export BetaBinomial
 import Base
 using SpecialFunctions
 
-
 @parameterized BetaBinomial(n, α, β)
 
 basemeasure(d::BetaBinomial) = CountingMeasure()
@@ -25,23 +24,20 @@ function Base.rand(
     rand(rng, Dists.BetaBinomial(d.n, d.α, d.β))
 end
 
-
 @inline function insupport(d::BetaBinomial, x)
     isinteger(x) && 0 ≤ x ≤ d.n
 end
 
 @inline function logdensity_def(d::BetaBinomial{(:n, :α, :β)}, y)
     (n, α, β) = (d.n, d.α, d.β)
-    logbinom = - log1p(n) - logbeta(y + 1, n - y + 1)
-    lognum   = logbeta(y + α, n - y + β)
+    logbinom = -log1p(n) - logbeta(y + 1, n - y + 1)
+    lognum = logbeta(y + α, n - y + β)
     logdenom = logbeta(α, β)
     return logbinom + lognum - logdenom
 end
 
-
 asparams(::Type{<:BetaBinomial}, ::StaticSymbol{:α}) = asℝ₊
 asparams(::Type{<:BetaBinomial}, ::StaticSymbol{:β}) = asℝ₊
-
 
 function proxy(d::BetaBinomial{(:n, :α, :β),Tuple{I,A}}) where {I<:Integer,A}
     Dists.BetaBinomial(d.n, d.α, d.β)
