bfdad6913b024b89720afe04ac4359afc9d42783
Chad Scherrer <chad.scherrer@gmail.com>
2025-01-14 08:00:33 -0800

    qualify massof

--- src/combinators/affine.jl
+++ src/combinators/affine.jl
@@ -20,7 +20,7 @@ function Pretty.tile(f::AffineTransform)
     result
 end
 
-massof(f::AffineTransform) = massof(parent(f))
+MeasureBase.massof(f::AffineTransform) = massof(parent(f))
 
 Base.show(io::IO, f::AffineTransform) = Pretty.pprint(io, f)
 
